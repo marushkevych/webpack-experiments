99f2c9a1f0f01004e09c8b92912787ccae611d71
Andrey Marushkevych <marushkevych@gmail.com>
2018-02-22 17:53:34 -0500

    use compression-webpack-plugin to generate gziped bundle

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 const config = {
   devtool: 'source-map',
@@ -19,6 +20,16 @@ const config = {
     new ExtractTextWebpackPlugin('style.css'),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true // dont remove sourceMap footer from the bundle
+    }),
+    // this will generate bundle.js.gz. The server needs to be configured
+    // to serve bundle.js.gz instead of bundle.js (if browser supports it)
+    // see https://stackoverflow.com/a/34828216/852675
+    new CompressionWebpackPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css)$/,
+      threshold: 0,
+      minRatio: 0.8
     })
   ]
 }
