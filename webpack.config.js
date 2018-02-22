033f45f621d86143c8774925a896ba8d2790516c
Andrey Marushkevych <marushkevych@gmail.com>
2018-02-22 14:05:04 -0500

    addons

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const commonConfig = require('./build-utils/webpack.common')
 const webpackMerge = require('webpack-merge')
 
+const addons = addonsArg => {
+
+  // if arg is String - convert ot array and remove undefined values
+  const addons = []
+    .concat.call([], addonsArg)
+    .filter(Boolean)
+
+  return addons.map(addonName => require(`./build-utils/addons/webpack.${addonName}.js`))
+}
+
 module.exports = (env) => {
 
   console.log('env:', env)
 
   const envConfig = require(`./build-utils/webpack.${env.env}.js`)
 
-  return webpackMerge(commonConfig, envConfig)
+  const mergedConfig = webpackMerge(commonConfig, envConfig, ...addons(env.addons))
+
+  console.log(mergedConfig)
+
+  return mergedConfig
 }
