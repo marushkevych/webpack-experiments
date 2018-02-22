9633ccd75addcf7b2923fe880c369ca246747eab
Andrey Marushkevych <marushkevych@gmail.com>
2018-02-22 17:21:03 -0500

    extracting css in PROD

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,8 +1,22 @@
 const webpack = require('webpack')
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
 
 const config = {
-  devtool: "source-map",
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        // extract css into style.css (see plugins config below)
+        use: ExtractTextWebpackPlugin.extract({
+          use: 'css-loader', // this enables importing css (import './index.css')
+          fallback: 'style-loader' // for lazy loaded bundles - fall back to embedding css into html 
+        })
+      }
+    ]
+  },
   plugins: [
+    new ExtractTextWebpackPlugin('style.css'),
     new webpack.optimize.UglifyJsPlugin()
   ]
 }
